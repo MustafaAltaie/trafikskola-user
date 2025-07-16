e7a320437835875a5c44aace05376a6faa98424a
mustafa altaie <mustafaphoto111@gmail.com>
2025-07-16 12:31:52 +0200

    removed unused routes

--- src/app/api/education/route.js
+++ src/app/api/education/route.js
@@ -1,14 +1,6 @@
 import Education from '../../../../lib/models/Education';
 import dbConnect from '../../../../lib/mongodb';
 
-export async function POST(req) {
-  await dbConnect();
-  const data = await req.json();
-  const newItem = new Education(data);
-  await newItem.save();
-  return new Response(JSON.stringify(newItem), { status: 201 });
-}
-
 export async function GET() {
   await dbConnect();
   const items = await Education.find({});
