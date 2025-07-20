a2b98452980959cb5febded424c9a206be52b395
mustafa altaie <mustafaphoto111@gmail.com>
2025-07-20 21:03:41 +0200

    text

--- src/app/omOss/page.jsx
+++ src/app/omOss/page.jsx
@@ -18,22 +18,23 @@ const AboutPage = () => {
             <Header />
             <div className="aboutPageWrapper navPage">
                 <div>
-                    <h1>Katrineholm Trafikskola</h1>
+                    <h1>Eskilstuna Trafikskola i Katrineholm</h1>
                     <br />
                     <p>Vårt mål är att du skall få en bra och effektiv utbildning. Vi strävar alltid efter en effektiv utbildning för varje individ. Att gå till en trafikskola och få sin körkortsutbildning innebär att man får en professionell inlärning.</p>
                     <br />
-                    <p>Vi på <span>Katrineholm Trafikskola</span> AB utbildar bara inom behörighet B-körkort. För oss är anpassbarhet i körkortsutbildning en viktig grund för goda resultat.</p>
+                    <p>Vi på <span>Eskilstuna Trafikskola</span> AB utbildar bara inom behörighet B-körkort. För oss är anpassbarhet i körkortsutbildning en viktig grund för goda resultat.</p>
                     <br />
                     <p>Vi bemöter alla med förtroende och respekt. Vi tar gemensamt och personligt ansvar för din körkortsutbildning.</p>
                     <br />
                     <p>Vi hjälper dig med att planera din utbildning så att du lär dig det mesta möjliga på kortast möjliga tid.</p>
                     <br />
-                    <p>Vi arbetar efter en given undervisningsplan. Kontakta oss på <span>Katrineholm Trafikskola</span>, så kan vi tillsammans kolla på hur just du ska göra för att nå målet med körkortet.</p>
+                    <p>Vi arbetar efter en given undervisningsplan. Kontakta oss på <span>Eskilstuna Trafikskola</span>, så kan vi tillsammans kolla på hur just du ska göra för att nå målet med körkortet.</p>
                     <br />
                     <p>Du skall vara 16 år innan du får börja övningsköra. För bästa möjliga resultat rekommenderar vi att du börjar tidigt.</p>
                     <br />
                     <p>Kombinera gärna körningen på skolan med mängdträning hemma med din handledare. Givetvis har vi även intensivkurser för den som vill bli klar på kortare tid.</p>
                     <br />
+                    <h3 style={{ fontWeight: 'bold' }}>Vi har två lokaler, huvudlokal i Eskilstuna och andra lokal i Katrineholm</h3>
                     {/* <div className="aboutPageImageWrapper">
                         {images.map((url, idx) => (
                             <motion.div
